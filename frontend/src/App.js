e08242dbde3e1eedbe4e83eade954314e7bec6b5
amira791 <ka_bellali@esi.dz>
2023-08-29 19:13:28 +0200

    new backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import TopNavBar from './components/TopNavBar';
+import SideBar from './components/SideBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <TopNavBar />
+      <div className="content-container">
+        <Sidebar />
+        
+      </div>
     </div>
   );
 }
