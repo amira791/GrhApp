7dbcf0b21611acc4768baa5e532da246434bd7b4
Soumeya <ks_mekki@esi.dz>
2023-11-02 20:25:34 +0100

    Creating User model , repo , controller and service

--- backend/src/main/java/com/cnrc/grh/repository/UtilisateruRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cnrc.grh.repository;
-
-import com.cnrc.grh.model.Utilisateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtilisateruRepository extends JpaRepository<Utilisateur,String> {
-}
