d60591d7b53a22e03197d142c8da72e3435cbf42
Soumeya <ks_mekki@esi.dz>
2023-11-04 08:14:51 +0100

    Added Auth context to manage the state of the application

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { createContext , useReducer  } from "react";
+
+
+
+export const  AuthContext = createContext();
+
+
+
+export const authReducer = (state , action) => {
+    switch(action.type){
+        case 'LOGIN' :
+            return { 
+                ...state,
+                isAuthenticated: true,
+                token: action.payload,
+            }
+        case 'LOGOUT' :
+            return {
+                ...state,
+                isAuthenticated: false,
+                token: null,
+            }
+        default :
+            return state
+    }
+}
+export const AuthContextPrivider = ({children}) => {
+
+   const [state,dispatch] = useReducer(authReducer , {
+    isAuthenticated: false,
+    token: null,
+   });
+
+   useEffect(()=>{
+    
+     const token = localStorage.getItem('jwtToken');
+     console.log('Token from localStorage:', token);
+     if (token) {
+        dispatch({type : 'LOGIN' , payload: token})
+     }
+   },[])
+
+   console.log('AuthContext state:' , state);
+
+   return(
+     <AuthContext.Provider value={{ ...state , dispatch }}>
+        { children }
+    </AuthContext.Provider>
+   )
+}
+
+
