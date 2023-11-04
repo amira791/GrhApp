3ba9a7e2e26b3e14af764c72ac316dba75e699d4
Soumeya <ks_mekki@esi.dz>
2023-11-04 08:04:22 +0100

    Added Token management function in front

--- frontend/src/hooks/useJwt.jsx
+++ frontend/src/hooks/useJwt.jsx
@@ -1,25 +1,19 @@
 import { useEffect } from "react";
 
 export default function useJwt() {
-  // todo implement the login and sign up methods to receive the token then store it here
-  // to store it return setToken(token) and call it in login method
-  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyMSIsImlhdCI6MTY5OTAwMjgxMywiZXhwIjoxNjk5NjA3NjEzfQ.KeXCo3vj48Ozosrlnd3F1AeaK9D2XBsov05-0HqoqNE" ;
-  // const setToken = () => {
-  //   localStorage.setItem('jwtToken', token);
-  // };
-   
-  
-//   useEffect(() => {
-//     setToken();
-// }, [])
  
+  const setToken = (token) => {
+    localStorage.setItem('jwtToken', token);
+  };
+   
+   
   const getToken = () => {
     return localStorage.getItem('jwtToken');
   };
 
 
 const createHeaders = () => {
-    // const token = getToken();
+    const token = getToken();
     if (token) {
       return {
         'Content-Type': 'application/json',
@@ -31,12 +25,15 @@ const createHeaders = () => {
     };
   };
 
-//   const logout = () => {
-//     localStorage.removeItem('jwtToken');
+  const deleteToken = () => {
+    localStorage.removeItem('jwtToken');
    
-//   };
+  };
   return {
-    createHeaders
+    createHeaders,
+    setToken,
+    getToken,
+    deleteToken
   };
   
 }
